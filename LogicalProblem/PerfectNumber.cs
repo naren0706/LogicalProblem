f8a94db3ff243866f118f4739d7b7ff296f14dc2
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-13 15:27:28 +0530

    [Narenthra Kishore] Add: new Solution PerfectNumber

---
+++ LogicalProblem/PerfectNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblem
+{
+    class PerfectNumber
+    {
+        public static void IsPerfect()
+        {
+            Console.WriteLine("Enter the number ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for(int i = 1; i < Convert.ToInt32(Math.Sqrt(input)); i++)
+            {
+                if (input % i == 0)
+                {
+                    sum += i + Convert.ToInt32(input / i);
+                }
+            }
+            if(input==sum-input)
+            {
+                Console.WriteLine("it is a Perfect number ");
+            }
+            else { 
+                Console.WriteLine("It is not a perfect number");
+            }
+        }
+    }
+}
--- LogicalProblem/Program.cs
+++ LogicalProblem/Program.cs
@@ -7,12 +7,15 @@ namespace LogicalProblems
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter the input :");
-            Console.WriteLine("1.Fibonacci series");
+            Console.WriteLine("1.Fibonacci series\n 2. PerfectNumber");
             int input = Convert.ToInt32(Console.ReadLine());
             switch (input)
             {    
                 case 1:
                     Fibonacci.Fib();
+                    break;
+                case 2:
+                    PerfectNumber.IsPerfect();
                     break;
                 default:
                     break;
