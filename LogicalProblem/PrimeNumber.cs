07e8ca279e3aac2ae84cc3b70df9918f364954f3
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-13 15:40:46 +0530

    [Narenthra Kishore] Add: new solution PrimeNUmbers

---
+++ LogicalProblem/PrimeNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblem
+{
+    internal class PrimeNumber
+    {
+        public static void IsPrime()
+        {
+            Console.WriteLine("Enter a number to find prime or not ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            bool flag = true;
+            for(int i = 2; i <= Convert.ToInt32(Math.Sqrt(input))+1; i++)
+            {
+                if(input%i == 0)
+                {
+                    flag = false;
+                    break;
+                }
+                
+            }
+            if(flag)
+            {
+                Console.WriteLine("Prime Number");
+            }
+            else 
+            {
+                Console.WriteLine("Not a prime number");
+            }
+        }
+    }
+}
--- LogicalProblem/Program.cs
+++ LogicalProblem/Program.cs
@@ -7,7 +7,7 @@ namespace LogicalProblems
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter the input :");
-            Console.WriteLine("1.Fibonacci series\n 2. PerfectNumber");
+            Console.WriteLine("1.Fibonacci series\n 2. PerfectNumber\n 3.PrimeNUmber");
             int input = Convert.ToInt32(Console.ReadLine());
             switch (input)
             {    
@@ -16,6 +16,9 @@ namespace LogicalProblems
                     break;
                 case 2:
                     PerfectNumber.IsPerfect();
+                    break;
+                case 3:
+                    PrimeNumber.IsPrime();
                     break;
                 default:
                     break;
