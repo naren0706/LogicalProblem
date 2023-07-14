110e5b6ac6c986e89b6465f654357af8b15c153f
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-14 12:25:38 +0530

    [Narenthra Kishore] Add : New Solution for Temperature Conversion

--- LogicalProblem/Program.cs
+++ LogicalProblem/Program.cs
@@ -9,7 +9,7 @@ namespace LogicalProblems
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter the input :");
-            Console.WriteLine("1.Fibonacci series\n2.PerfectNumber\n3.PrimeNUmber\n4.Reverse a number\n5.coupon Number\n6.Stopwatch\n7.Vending Meachine\n8.Date to day");
+            Console.WriteLine("1.Fibonacci series\n2.PerfectNumber\n3.PrimeNUmber\n4.Reverse a number\n5.coupon Number\n6.Stopwatch\n7.Vending Meachine\n8.Date to day\n9.Temperature conversion");
             int input = Convert.ToInt32(Console.ReadLine());
             switch (input)
             {    
@@ -37,6 +37,10 @@ namespace LogicalProblems
                 case 8:
                     DayOfDate.getDay();
                     break;
+                case 9:
+                    Temperature.ConvertTemp();
+                    break;
+
 
                 default:
                     break;
---
+++ LogicalProblem/Temperature.cs
@@ -0,0 +1,23 @@
+﻿namespace LogicalProblems
+{
+    internal class Temperature
+    {
+        internal static void ConvertTemp()
+        {
+            Console.WriteLine("1.celcius to Fahrenheit:\n2.Fahrenheit to celcius:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if(choice==1)
+            {
+                Console.WriteLine("Enter the value of celcius");
+                int input = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(input*(9/5)+32);
+            }
+            if(choice==2)
+            {
+                Console.WriteLine("Enter the valu of Fahrenheit");
+                int input = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine((input-32)*(5/9));
+            }
+        }
+    }
+}
